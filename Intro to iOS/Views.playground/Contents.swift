98290a3b1b1a3398b3389fb1c8c1501afe30cbd2
Kathryn Hodge <kahodge@vassar.edu>
2016-01-06 16:06:29 -0800

    iOS continued!

--- Intro to iOS/Views.playground/Contents.swift
+++ Intro to iOS/Views.playground/Contents.swift
@@ -9,3 +9,31 @@ let spinner = UIImage(named: "spinner")
 // Bounds (frame)
 // Center
 let imageView = UIImageView(frame: CGRect(origin: CGPointZero, size: spinner!.size))
+imageView.image = spinner
+
+imageView
+
+imageView.bounds.size
+
+let backgroundView = UIView(frame:CGRect(x: 0, y: 0, width: 300, height: 300))
+
+backgroundView.backgroundColor = UIColor.orangeColor()
+
+backgroundView.addSubview(imageView)
+imageView.center = backgroundView.center
+backgroundView
+
+XCPlaygroundPage.currentPage.liveView = backgroundView
+
+let animation = CABasicAnimation(keyPath: "transform.rotation")
+
+animation.fromValue = 0.0
+
+animation.toValue = 2 * M_PI
+
+animation.duration = 1.0
+
+animation.repeatCount = 15
+
+imageView.layer.addAnimation(animation, forKey: "transform.rotation")
+
